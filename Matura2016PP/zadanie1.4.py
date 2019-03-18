c14a72a19c17179c5639a7168aabe7d0ffe6ab77
PRagus19 <pragus36@gmail.com>
2019-03-18 13:15:18 +0100

    Zadanie2.1matura2016PP
    Zadanie 1 i zadanie 2.1 matura 2016PP

---
+++ Matura2016PP/zadanie1.4.py
@@ -0,0 +1,22 @@
+"""
+Matura 2016 PP Zadanie 1.4
+(C) Patryk Ragus, marzec 2019r.
+Lo w Sobolewie
+"""
+n = 10
+T = 'qqqwwweeee'
+# b = ? rozmiar skompresowanego tekstu
+TS = ''
+az = T[0]
+i = 0
+for z in T:
+    if z == az:
+        i += 1
+    else:
+        TS = TS+str(i)+az
+        az = z
+        i = 1
+TS = TS + str(i) + z
+b = len(TS)
+print(TS)
+print(b)
