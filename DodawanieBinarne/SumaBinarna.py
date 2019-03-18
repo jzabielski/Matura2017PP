2b05f47c434b1dfc0512cd8e06be56750267e55b
jzabielski <zabielskijarek@gmail.com>
2019-03-18 11:57:45 +0100

    matura2016PP
    matura2016PP arkusze + dane

--- DodawanieBinarne/SumaBinarna.py
+++ DodawanieBinarne/SumaBinarna.py
@@ -13,12 +13,14 @@ def suma2(L1, L2):
     while len(L2)<len(L1):
         L2 = L2 + '0'
     for i in range(len(L1)):
-        z1 = int(L1[i])
-        z2 = int(L2[i])
-        sCyfr = (dalej + z1 + z2)%2
+        sCyfr = dalej + int(L1[i]) + int(L2[i])
         if sCyfr>1:
             dalej = 1
-        w = w + str(sCyfr)
+        else:
+            dalej = 0
+        w = w + str(sCyfr%2)
+    if dalej==1:
+        w = w + '1'
     w = w[::-1]
     return w
 print(suma2('101', '11101'))
