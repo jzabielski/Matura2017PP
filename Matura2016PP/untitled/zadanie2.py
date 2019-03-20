493205e8df73e565294d7b7b7695b09ceb5aac1d
izalapacz <45933481+izalapacz@users.noreply.github.com>
2019-03-20 13:34:52 +0100

    zadanie4matura2016pp
    zadanie4matura2016pp

---
+++ Matura2016PP/untitled/zadanie2.py
@@ -0,0 +1,16 @@
+"""
+ Zadanie 2 matura 2016 PP
+ (C) Izabela Lapacz, marezec 2019r.
+ LO w Sobolewie
+"""
+def suma_cyfr(n):
+    n = 123456
+    SumaCyfr = 0
+    while n>0:
+        SumaCyfr = SumaCyfr + n%10
+        n = n//10
+    return(SumaCyfr)
+n = 123456
+print(suma_cyfr(n))
+k = (n - suma_cyfr(n)) /9
+print(k)
