0866cbe410929225c94683bced43047d014a162b
PRagus19 <pragus36@gmail.com>
2019-03-27 12:59:47 +0100

    matrura2016PPzadanie6
    matura2016PPzadanie6

---
+++ Matura2016PP/zadanie6matura2016PP/zadanie6.py
@@ -0,0 +1,23 @@
+"""
+Zadanie 6 matura 2016PP
+(C) Patryk Ragus, marzec 2019r.
+LO w Sobolewie
+"""
+import math as m
+def pierwsza(n):
+    czy_pierwsza = True
+    for i in range(2,int(m.sqrt(n))+1,1):
+        if n%i == 0:
+            czy_pierwsza = False
+    return czy_pierwsza
+
+with open ("dane6.txt", "r") as plik:
+    licznik1 = 0
+    for d in plik:
+        d = d.strip()
+        d = int(d)
+        if pierwsza(d):
+            licznik1 += 1
+        print(d)
+with open("wyniki_6.txt", "w") as w:
+    w.write("6.1\n"+str(licznik1)+"\n")
