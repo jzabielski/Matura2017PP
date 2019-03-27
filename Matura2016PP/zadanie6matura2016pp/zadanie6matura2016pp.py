23466aea1419c899649d7fef53bcdd3f960805a8
Jelonek17 <45925012+Jelonek17@users.noreply.github.com>
2019-03-27 13:33:03 +0100

    zadanie6matura2016pp
    zadanie 6 matura 2016 pp

---
+++ Matura2016PP/zadanie6matura2016pp/zadanie6matura2016pp.py
@@ -0,0 +1,26 @@
+"""
+Zadanie 6 matura 2016 PP
+(C) Karolina Dudkowska, marzec 2019
+LO w Sobolewie
+"""
+import math as m
+def pierwsza(n):
+    czy_pierwsza = True
+    for i in range(2, int(m.sqrt(n))+1, 1):
+        if n%i == 0:
+            czy_pierwsza = False
+    return czy_pierwsza
+
+
+with open("dane6.txt", "r") as plik:
+    licznik1 = 0
+    min1 = 30001
+    max1 = 0
+    for d in plik:
+        d = d.strip()
+        d = int(d)
+        if pierwsza(d):
+            licznik1+=1
+        print(d)
+with open("wyniki_6.txt", "w") as w:
+    w.write("6.1\n"+str(licznik1)+"\n")
