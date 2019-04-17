be2bf2608f63d1504189d93b3b17aa7271b70b08
jgoliszewksi <jakub.goliszewksi01@gmail.com>
2019-04-17 13:09:44 +0200

    zadanie5matura2015PP

--- Matura2015PP/zadanie5/zadanie5.py
+++ Matura2015PP/zadanie5/zadanie5.py
@@ -4,9 +4,30 @@ Zadanie 5 matura 2015PP
 LO Sobolew
 """
 N = []
+slowa = []
+nowe = []
 for i in range(12):
     N.append(0)
-with open("slowa.txt","r") as plik:
-    for slowo in plik:
-        slowo = slowo.strip()
-        N[len(slowo)-1] += 1
+with open("slowa.txt","r") as plik, open('nowe.txt','r') as plik2:
+    for s in plik:
+        s = s.strip()
+        N[len(s)-1] += 1
+        slowa.append(s)
+    for n in plik2:
+        n = n.strip()
+        nowe.append(n)
+with open('wynik5.txt','w+') as wynik:
+    wynik.write("5.1\n")
+    for i in range(12):
+        wynik.write(str(i+1)+' '+str(N[i])+"\n")
+    wynik.write("\n5.2\n")
+    for n in nowe:
+        wynik.write(n+' ')
+        l_wyst = 0
+        l_wyst_odbicia = 0
+        for s in slowa:
+            if n == s:
+                l_wyst += 1
+            if n[::-1] == s:
+                l_wyst_odbicia += 1
+        wynik.write(str(l_wyst) + ' ' + str(l_wyst_odbicia) + "\n")
