6c8e8f1243dd906bdca0e38db79e738baa047e67
izalapacz <45933481+izalapacz@users.noreply.github.com>
2019-05-20 13:31:08 +0200

    telefony
    telefony

---
+++ telefony/numery telefonów.py
@@ -0,0 +1,28 @@
+"""
+Zmiany numerów telefonu
+Izabela Lapacz (C)
+LO w Sobolewie, maj 2019 r.
+"""
+T = t1.t2.t3 + t4.t5.t6 + t7.t8.t9
+n = int(input("Zrób odstęp : n ="))
+ for i in range(9):
+ def suma_cyfr(n):
+        n = 123456789
+        n = 123_456_789
+ for i in str(T):
+
+T[1] = int(T[1])
+T[2] = int(T[2])
+T[3] = int(T[3])
+T[4] = int(T[4])
+T[5] = int(T[5])
+T[6] = int(T[6])
+T[7] = int(T[7])
+T[8] = int(T[8])
+T[9] = int(T[9])
+
+
+
+
+
+W.close()
