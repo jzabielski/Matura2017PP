f19fb3c13bbe45896f3eccb4a7a1c8876ea9997c
czaro506 <czaro506@gmail.com>
2019-06-03 12:33:41 +0200

    matura_2015PP

---
+++ Matura2014PP/zadanie5/zadanie5matura2014PP.py
@@ -0,0 +1,34 @@
+"""
+Zadanie 5 matura 2014 PP
+(C) Cezary Winek
+LO im. T. KOściuszki w Sobolewie
+"""
+
+def wielokrotne(a, b):
+    wiel = False
+    if a!=0 and b!=0:
+        if a%b == 0 or b%a == 0:
+            wiel = True
+    return wiel
+def wzglednie_pierwsze(a, b)
+    wzgl_p = True
+    min_ab = min(a, b)
+    for i in range(2, min_ab+1):
+        if a%i == 0 and b%i == 0:
+            wzgl_p = False
+    return wzgl_p
+
+with open("PARY_LICZB.TXT",'r') as plik:
+    licznik_a = 0
+    for i in plik:
+        i = i.strip()
+        liczba1, liczba2 = i.split(" ")
+        liczba1 = int(liczba1)
+        liczba2 = int(liczba2)
+        print(f"{liczba1} - {liczba2}")
+        if wielokrotne(liczba1, liczba2):
+            licznik_a+=1
+    with open("ZADANIE5.TXT", 'w') as w:
+        w.write("a)\n"
+                + str(licznik_a)
+                + "\n")
